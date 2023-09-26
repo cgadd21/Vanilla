43648c5b99da5f6992275635e4466b7796e9db95
cgadd21 <126418690+cgadd21@users.noreply.github.com>
2023-09-25 21:03:53 -0400

    added github to footer

--- inc/footer.php
+++ inc/footer.php
@@ -2,5 +2,6 @@
     <ul class="flexd">
         <li><a target="_blank" href="https://www.linkedin.com/in/coopergadd/">LinkedIn</a></li>
         <li><a target="_blank" href="mailto:cooper@ctgadd.com">Email</a></li>
+        <li><a target="_blank" href="https://github.com/cgadd21">GitHub</a></li>
     </ul>
 </footer>
