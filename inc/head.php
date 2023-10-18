811bac1922b4e8618df85676912a258e0f66b69d
cgadd21 <126418690+cgadd21@users.noreply.github.com>
2023-10-18 13:58:27 -0400

    Added favicons and edited css

--- inc/head.php
+++ inc/head.php
@@ -4,4 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
     <title><?php echo $currentPage; ?> | Cooper Gadd</title>
+    <link rel="icon" type="image/png" href="favicon.png" />
+    <link rel="apple-touch-icon" sizes="512x512" href="icon-512.png" />
+    <link rel="apple-touch-icon" sizes="192x192" href="icon-192.png" />
 </head>
